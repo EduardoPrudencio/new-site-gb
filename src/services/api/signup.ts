86f23c9ba3ac1ff80e3157d533e7d12d8975f15d
Eduardo Lobo <eduardo@Eduardos-MacBook-Pro.local>
2022-07-03 20:00:16 -0300

    Limpando a api de assinatura

--- src/services/api/signup.ts
+++ src/services/api/signup.ts
@@ -1,20 +1,5 @@
-import { apiDefault } from ".";
-
-export const SignUpCall = async (values) => {
+export const SignUpCall = async () => {
     
-    try {    
+            
         
-        const {status} = await apiDefault .post('v2/customers',{
-            customer: {
-                nickname: values.name, 
-                email: values.email, 
-                password: values.password, 
-                mkt: values.receiveAboutProductsAndServices ? '1' : '0' 
-            },
-            voucher: values.voucher,
-        });
-
-        return status;
-   
-    } catch (error) {}
 };
