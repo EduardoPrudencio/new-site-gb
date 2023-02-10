758e6cf44f08c5c3c8fd48a764a1a5e07ed3981b
Eduardo Prudencio <eduardolobo.esp@gmail.com>
2023-02-10 17:10:25 -0300

    Ajustes para produção

--- src/services/GymManager.ts
+++ src/services/GymManager.ts
@@ -2,8 +2,8 @@ import { Gym } from "../model/models";
 
 export const gyns: Gym[] = [
   {
-    id: "57ab02bf-04fe-4db4-a1e1-a5947298e70d",
-    activityId: "58e3ca3a-7ea7-4ed7-ba89-f1f7dad12a28",
+    id: "d1bc6916-45ef-43e3-9c37-c33f90c02d2b",
+    activityId: "29b0e62f-c75b-42d8-99e2-9c2dfc839edc",
     name: "Campos dos Goytacazes",
     slug: "campos-dos-goytacazes",
     isDefault: true,
